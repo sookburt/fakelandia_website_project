7e3c7928b3d511f5dad04a25816a46aef6630dbe
Sue Burt <sookburt@gmail.com>
2022-04-22 18:52:43 +0100

    Create misdemeanour page, call api, add filter

--- src/components/Misdemeanour.tsx
+++ src/components/Misdemeanour.tsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
+import generateMisdemeanours, {MisdemeanourRecord} from "../data/generate_misdemenours";
 
-const Misdemeanour: React.FC = () => (
+
+
+const Misdemeanour: React.FC = () => {
+
+  const [filter, setFilter] = useState(10);
+  const updateFilter = (value: string) => {
+    const num = parseInt(value);
+    if(num !== filter){
+      setFilter(num);
+    }
+  };
+  const [misdemeanours, setMisdemeanours] = useState<MisdemeanourRecord[]>([]);
+
+
+  useEffect(() =>  {
+    const callApi = async () => {
+      setMisdemeanours(await generateMisdemeanours(filter));
+    }
+    callApi();
+  }, [filter])
+
+  return (
     <>
-      <div>Misdemeanour</div>
+      <h1>Misdemeanours</h1>
+      {console.log(misdemeanours)}
+      <label className="label--hidden" htmlFor='myFilter'>Filter</label>
+      <select 
+        id='myFilter' 
+        onChange={(e) => {e.preventDefault();updateFilter(e.target.value)}}>
+        <option value='10'>Get Ten</option>
+        <option value='1'>Get One</option>
+      </select>
     </>
   );
+}
 
 export default Misdemeanour;
