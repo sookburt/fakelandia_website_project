6f9ad419b8ed02bd5ea83aec859551d431f48646
Sue Burt <sookburt@gmail.com>
2022-04-22 15:37:20 +0100

    Added text to home, basic css

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,13 @@
 
-const Home: React.FC = () => <div>Home!</div>;
+const Home: React.FC = () => (
+  <>
+    <article>
+      <h1>Welcome to the home of the Justice Department of Fakelandia.</h1>
+      <p>Here you can browse a list of recent misdemeanours committed by our citizens,
+        or you can confess to your own misdemeanour.
+      </p>
+    </article>
+  </>
+);
 
 export default Home;
